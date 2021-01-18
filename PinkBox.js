1f7f3125df5eb97bc86f1111646bd7e028987a14
KhadijaHasanova <74422542+KhadijaHasanova@users.noreply.github.com>
2021-01-18 17:14:42 -0600

    Update PinkBox.js

--- PinkBox.js
+++ PinkBox.js
@@ -1,10 +1,9 @@
 class PinkBox {
     constructor(x, y, width, height) {
       var options = {
-          'restitution':0.8,
-          'friction':1,
-          'density':1.0
-      }
+        'restitution':0.4,
+        'friction':0,
+    }
       this.body = Bodies.rectangle(x, y, width, height, options);
       this.width = width;
       this.height = height;
